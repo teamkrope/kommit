9af8c13833cc4bede4e1d0efdd94f7fb4bcc92a8
huzaifaasim017 <mohammadhuzaifa117@gmail.com>
2023-10-21 12:12:09 +0500

    .

--- main.py
+++ main.py
@@ -12,19 +12,27 @@ def create_random_commit():
     # Generate a random key for the commit message
     random_key = ''.join(random.choice('0123456789ABCDEF') for _ in range(8))
 
+    # Get the current SHA of the file
+    headers = {
+        'Authorization': f'token {access_token}'
+    }
+    file_url = f'https://api.github.com/repos/{username}/{repository}/contents/random_key.txt'
+    response = requests.get(file_url, headers=headers)
+    current_file_info = response.json()
+    current_sha = current_file_info['sha']
+
     # Create a new commit
     commit_message = f"Commit with random key: {random_key}"
     data = {
         'message': commit_message,
         'content': random_key,
         'branch': branch,
+        'sha': current_sha  # Provide the current SHA
     }
 
-    headers = {
-        'Authorization': f'token {access_token}'
-    }
-
-    response = requests.put(f'https://api.github.com/repos/{username}/{repository}/contents/random_key.txt', headers=headers, json=data)
+    response = requests.put(file_url, headers=headers, json=data)
+    print(f"Response status code: {response.status_code}")
+    print(f"Response content: {response.text}")
 
     if response.status_code == 200:
         print(f"Commit with random key {random_key} created successfully.")
@@ -33,4 +41,4 @@ def create_random_commit():
 
 while True:
     create_random_commit()
-    time.sleep(60)  # Sleep for 60 seconds before the next commit
+    time.sleep(50)  # Sleep for 5 seconds before the next commit
